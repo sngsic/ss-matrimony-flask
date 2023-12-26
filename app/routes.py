f9e49c7ba7b8c759747a08dd228e99507615b9ef
sngsic <sngcetsic@gmail.com>
2023-12-26 16:49:03 +0530

    corrected bug that causes internal server error in index.html

--- app/routes.py
+++ app/routes.py
@@ -3,21 +3,21 @@ from app.forms import LoginForm
 from flask import render_template, flash, redirect, url_for
 
 
-@app.route("/")
-@app.route("/index")
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
 def index():
     user = {"username": "name"}
     posts = [
         {"author": {"username": "John"}, "body": "Hey there"},
         {"author": {"username": "Mike"}, "body": "Welcome"},
     ]
-
-    return render_template("index.html", title="Home", user=user, posts=posts)
+    form = LoginForm()
+    return render_template("index.html", title="Home", user=user, posts=posts, form=form)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    form = LoginForm()
+    
     if form.validate_on_submit():
         flash(
             "Login requested for {}, remember me = {}".format(
@@ -25,4 +25,4 @@ def login():
             )
         )
         return redirect(url_for("index"))
-    return render_template("login.html", title="Sign In", form=form)
+    return render_template("login.html", title="Sign In")
